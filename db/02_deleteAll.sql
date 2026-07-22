ac8b1e8fe7066830ff5864206b297ee83bff1235
Bruno <bkoren.it@gmail.com>
2026-07-22 23:34:37 +0200

    Author, Category, Source CRUD procs

--- db/02_deleteAll.sql
+++ db/02_deleteAll.sql
@@ -1,29 +1,32 @@
 USE NewsAppDb
 GO	
 
-CREATE PROCEDURE [dbo].[DeleteAllData]
+CREATE OR ALTER PROC [dbo].[p_DeleteAllData]
 AS
 BEGIN   
     SET NOCOUNT ON;
 	
+	BEGIN TRAN;        
+        
     BEGIN TRY                 
-        BEGIN TRANSACTION;
-            DELETE FROM [dbo].[ArticleAuthor];    
-            DELETE FROM [dbo].[ArticleCategory];   
-            DELETE FROM [dbo].[Article];                       
-			DELETE FROM [dbo].[Author];
-            DELETE FROM [dbo].[Category];
-            DELETE FROM [dbo].[Source];
-			
-            DBCC CHECKIDENT ('[dbo].[Article]',  RESEED, 0);
-            DBCC CHECKIDENT ('[dbo].[Author]',   RESEED, 0);
-            DBCC CHECKIDENT ('[dbo].[Category]', RESEED, 0);
-            DBCC CHECKIDENT ('[dbo].[Source]',   RESEED, 0);           
-        COMMIT TRANSACTION;
+		DELETE FROM [dbo].[ArticleAuthor];    
+        DELETE FROM [dbo].[ArticleCategory];   
+        DELETE FROM [dbo].[Article];                       
+		DELETE FROM [dbo].[Author];
+        DELETE FROM [dbo].[Category];
+        DELETE FROM [dbo].[Source];
+		
+        DBCC CHECKIDENT ('[dbo].[Article]',  RESEED, 0);
+        DBCC CHECKIDENT ('[dbo].[Author]',   RESEED, 0);
+        DBCC CHECKIDENT ('[dbo].[Category]', RESEED, 0);
+        DBCC CHECKIDENT ('[dbo].[Source]',   RESEED, 0);           
+        
+		COMMIT TRAN;
     END TRY
 
     BEGIN CATCH    
-		ROLLBACK TRANSACTION;
+		ROLLBACK TRAN;
+
         THROW;
     END CATCH
 END;
